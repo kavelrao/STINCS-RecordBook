eb1a4246b7395d01f349026ee2d37c6b46d454d9
Kavel Rao <rao.kavel@gmail.com>
2019-12-13 13:49:39 -0800

    Did a lot of stuff

--- recordbook/recordbook/urls.py
+++ recordbook/recordbook/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import sys
-import os
 
 from django.contrib import admin
 from django.urls import path, include
 
+# Import sibling folder 'home'
+import sys
+import os
 sys.path.append(os.path.abspath('../home'))
 import home
+
 
 urlpatterns = [
     path('home/', include('home.urls')),
